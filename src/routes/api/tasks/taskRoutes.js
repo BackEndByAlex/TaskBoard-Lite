14196b2b6547f7f803363b5c71cc88ae3e5d7ebd
Alexandru Cristian Antonescu <alexsept100@gmail.com>
2025-10-08 15:39:22 +0200

    delete files and maps.

--- src/routes/api/tasks/taskRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from 'express'
-
-const taskRoutes = express.Router()
-
-export default taskRoutes
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,5 @@
 import express from 'express'
 
-import authRoutes from './api/auth/authRoutes.js'
-import taskRoutes from './api/tasks/taskRoutes.js'
-import userRoutes from './api/users/userRoutes.js'
-
 import { 
   renderHomePage, 
   renderLoginPage, 
@@ -15,10 +11,5 @@ const router = express.Router()
 router.get('/', renderHomePage)
 router.get('/login', renderLoginPage)
 router.get('/register', renderRegisterPage)
-
-
-router.use('/auth', authRoutes)
-router.use('/tasks', taskRoutes)
-router.use('/users', userRoutes)
 
 export default router
