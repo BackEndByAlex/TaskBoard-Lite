7d2bd272c84fb568355c4822c5ca87402be760c5
Alexandru Cristian Antonescu <alexsept100@gmail.com>
2025-10-08 11:25:09 +0200

    structure the files.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,17 @@
 import express from 'express'
 
+import authRoutes from './api/auth/authRoutes.js'
+import taskRoutes from './api/tasks/taskRoutes.js'
+import userRoutes from './api/users/userRoutes.js'
+
 import { pageController } from '../controller/pageController.js'
 
 const router = express.Router()
 
-router.get('/', pageController)
+router.get('/', pageController) // home page route
 
-export { router }
+router.use('/auth', authRoutes)
+router.use('/tasks', taskRoutes)
+router.use('/users', userRoutes)
+
+export default router
